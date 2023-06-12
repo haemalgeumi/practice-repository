69aba09b4363c6ba8a873b5d28ea133f14621bcf
haemalgeumi <haemalgeumi2@gmail.com>
2023-06-12 17:28:42 +0900

    연습용임

---
+++ shop/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <
+
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
